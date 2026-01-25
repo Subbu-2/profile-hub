aa269ea71ead92819ee1e5e273f34b51cb1124e6
Subbu-2 <subbusubramanyam92@gmail.com>
2026-01-25 16:18:00 -0600

    add privacy,terms,footer

---
+++ src/components/privacy/index.jsx
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+const Privacy = () => {
+  return (
+    <div className="ph-legal">
+      <h1 className="ph-legal__title">Privacy</h1>
+      <p className="ph-legal__text">Coming soon.</p>
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ src/components/terms/index.jsx
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+const Terms = () => {
+  return (
+    <div className="ph-legal">
+      <h1 className="ph-legal__title">Terms</h1>
+      <p className="ph-legal__text">Coming soon.</p>
+    </div>
+  );
+};
+
+export default Terms;
