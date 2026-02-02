f643508d45f50bad432db7f677833ba11c1736ec
Subbu-2 <subbusubramanyam92@gmail.com>
2026-02-01 23:25:35 -0600

    typo fix

--- src/layouts/publicLayout/index.jsx
+++ src/layouts/publicLayout/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import ThemeHost from "../../components/public/ThemeHost";
+import ThemeHost from "../../components/public/themeHost";
 import "./index.scss";
 
 export default function PublicLayout() {
